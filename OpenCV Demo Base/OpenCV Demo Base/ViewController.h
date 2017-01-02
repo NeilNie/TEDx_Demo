3a5f23ce74c8285a97efc6d732eb26d9c9e0a799
NeilDeveloper <neil@nspiresoftware.net>
2017-01-02 12:38:26 -0500

    OpenCV demo base created, irrelevant classes removed.

---
+++ OpenCV Demo Base/OpenCV Demo Base/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenCV Demo Base
+//
+//  Created by Yongyang Nie on 1/2/17.
+//  Copyright © 2017 Yongyang Nie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
